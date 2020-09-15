00c92a25efc7b98aac89a4757cb25cc76fe08d92
VaughnHartling <Voonhartking@gmail.com>
2020-09-15 10:06:35 -0700

    Added config file to control elements such as the user list and connecting to new users.

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Set to true to connect with users not already in your network
+  connectWithUsers: false,
+  // Set to true to open the chromium browser to monitor the code
+  openBrowser: false,
+  // Add users to endorse
+  userList: [
+
+  ],
+};
